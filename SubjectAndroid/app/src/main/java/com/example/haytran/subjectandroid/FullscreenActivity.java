55dd72167922736341d218d49d6ccf790c377185
Van Hay <centertran2012@gmail.com>
2017-05-14 00:03:26 +0800

    Edit Android Subject project

--- SubjectAndroid/app/src/main/java/com/example/haytran/subjectandroid/FullscreenActivity.java
+++ SubjectAndroid/app/src/main/java/com/example/haytran/subjectandroid/FullscreenActivity.java
@@ -1,15 +1,22 @@
 package com.example.haytran.subjectandroid;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 /**
@@ -17,6 +24,7 @@ import android.widget.VideoView;
  * status bar and navigation/system bar) with user interaction.
  */
 public class FullscreenActivity extends AppCompatActivity {
+    private static final String TAG = FullscreenActivity.class.getSimpleName();
     private static final boolean AUTO_HIDE = true;
 
     /**
@@ -31,7 +39,6 @@ public class FullscreenActivity extends AppCompatActivity {
      */
     private static final int UI_ANIMATION_DELAY = 300;
     private final Handler mHideHandler = new Handler();
-    private View mContentView;
     private final Runnable mHidePart2Runnable = new Runnable() {
         @SuppressLint("InlinedApi")
         @Override
@@ -50,6 +57,7 @@ public class FullscreenActivity extends AppCompatActivity {
         }
     };
     private View mControlsView;
+    private View mContentView;
     private final Runnable mShowPart2Runnable = new Runnable() {
         @Override
         public void run() {
@@ -82,57 +90,174 @@ public class FullscreenActivity extends AppCompatActivity {
             return false;
         }
     };
-    Button btnChange;
+    ImageButton btnChangeActivity,btnPlayPause, btnFastRewind, btnFastForward, btnSound;
+    SeekBar seekBar;
     VideoView videoView;
+    TextView textViewTitleVideo;
     Uri path;
     int currentPosition = 0;
+    int videoDuration = 0;
+    String filename = "";
+    boolean isPlaying = true;
+    boolean isMuted = false;
+    private Handler threadHandler = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fullscreen);
+        mapping();
+        init();
+        addControls();
+    }
+
+    private void addControls() {
+            // Set up the user interaction to manually show or hide the system UI.
+        mContentView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggle();
+            }
+        });
+        btnChangeActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FullscreenActivity.this,MainActivity.class);
+                intent.addFlags(105);
+                intent.putExtra("path",path+"");
+                intent.putExtra("currentPosition",videoView.getCurrentPosition());
+                intent.putExtra("filename",filename);
+                startActivity(intent);
+                finish();
+            }
+        });
+        btnPlayPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isPlaying) {
+                    btnPlayPause.setImageResource(R.drawable.ic_play_arrow_black_24dp);
+                    videoView.pause();
+                } else  {
+                    btnPlayPause.setImageResource(R.drawable.ic_pause_circle_filled_black_24dp);
+                    videoView.seekTo(videoView.getCurrentPosition());
+                    videoView.start();
+                }
+                isPlaying = !isPlaying;
+            }
+        });
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                videoView.pause();
+                videoView.seekTo(seekBar.getProgress());
+                videoView.start();
+            }
+        });
+        btnFastRewind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentPosition = videoView.getCurrentPosition();
+                int SUBTRACT_TIME = 5000;
+                if(currentPosition - SUBTRACT_TIME > 0 )  {
+                    videoView.seekTo(currentPosition - SUBTRACT_TIME);
+                }
+            }
+        });
+        btnFastForward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentPosition = videoView.getCurrentPosition();
+                videoDuration = videoView.getDuration();
+                int ADD_TIME = 5000;
+                if(currentPosition + ADD_TIME < videoDuration)  {
+                    videoView.seekTo(currentPosition + ADD_TIME);
+                }
+            }
+        });
+        btnSound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isMuted = !isMuted;
+                if (isMuted) {
+                    AudioManager audioManager =(AudioManager)getSystemService(Context.AUDIO_SERVICE);
+                    try {
+                        audioManager.adjustVolume(AudioManager.ADJUST_MUTE,0);
+                    } catch (IllegalArgumentException ex) {
+                        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.ADJUST_MUTE, 0);
+                    }
+                    btnSound.setImageResource(R.drawable.ic_volume_up_black_24dp);
+                } else {
+                    AudioManager audioManager =(AudioManager)getSystemService(Context.AUDIO_SERVICE);
+                    try {
+                        audioManager.adjustVolume(AudioManager.ADJUST_UNMUTE,0);
+                    } catch (IllegalArgumentException ex) {
+                        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.ADJUST_UNMUTE, 0);
+                    }
+                    btnSound.setImageResource(R.drawable.ic_volume_off_black_24dp);
+                }
+            }
+        });
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                videoDuration = videoView.getDuration();
+                Log.d(TAG,"duration = " + videoDuration);
+                seekBar.setMax(videoDuration);
+            }
+        });
+            // Auto hide after request show from user
+        findViewById(R.id.contentView_FullScreenActivity).setOnTouchListener(mDelayHideTouchListener);
+    }
+
+    private void init() {
         mVisible = true;
-        mControlsView = findViewById(R.id.fullscreen_content_controls);
-        mContentView = findViewById(R.id.fullscreen_content);
-        btnChange = (Button)findViewById(R.id.dummy_button);
-        videoView = (VideoView)findViewById(R.id.videoViewFullScreen);
         Intent intent = getIntent();
-        currentPosition = intent.getIntExtra("currentPositionPort",0);
-        path = Uri.parse(intent.getStringExtra("pathPortToLand"));
+        currentPosition = intent.getIntExtra("currentPosition",0);
+        path = Uri.parse(intent.getStringExtra("path"));
+        filename = intent.getStringExtra("filename");
         if(path != null){
             videoView.setVideoURI(path);
             videoView.seekTo(currentPosition);
             videoView.start();
+            textViewTitleVideo.setText(filename);
+            UpdateSeekBarThread updateSeekBarThread = new UpdateSeekBarThread();
+            threadHandler.postDelayed(updateSeekBarThread,50);
+        } else {
+            Toast.makeText(this, "Có lỗi, vui lòng thử lại", Toast.LENGTH_SHORT).show();
         }
-        btnChange.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(FullscreenActivity.this,MainActivity.class);
-                intent.putExtra("pathLandToPort",path+"");
-                intent.putExtra("currentPositionLand",currentPosition);
-                startActivity(intent);
-            }
-        });
-        // Set up the user interaction to manually show or hide the system UI.
-        mContentView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                toggle();
-            }
-        });
 
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
     }
-
+    private void mapping() {
+        mControlsView = findViewById(R.id.contentControl_FullScreenActivity);
+        mContentView = findViewById(R.id.contentView_FullScreenActivity);
+        videoView = (VideoView)findViewById(R.id.videoView_FullScreenActivity);
+        btnChangeActivity = (ImageButton)findViewById(R.id.btnChangeActivity_FullScreenActivity);
+        btnFastForward = (ImageButton)findViewById(R.id.btnFastForward_FullScreenActivity);
+        btnFastRewind = (ImageButton)findViewById(R.id.btnFastRewind_FullScreenActivity);
+        btnPlayPause = (ImageButton)findViewById(R.id.btnPlayPause_FullScreenActivity);
+        btnSound = (ImageButton)findViewById(R.id.btnSound_FullScreenActivity);
+        seekBar = (SeekBar)findViewById(R.id.seekBar_FullScreenActivity);
+        textViewTitleVideo = (TextView)findViewById(R.id.textViewVideoTitle_FullScreenActivity);
+    }
+        // Thread used to update for seekbar
+    class UpdateSeekBarThread implements Runnable {
+        public void run()  {
+            seekBar.setProgress(videoView.getCurrentPosition());
+            threadHandler.postDelayed(this, 50);
+        }
+    }
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
         delayedHide(100);
     }
 
@@ -145,7 +270,7 @@ public class FullscreenActivity extends AppCompatActivity {
     }
 
     private void hide() {
-        // Hide UI first
+            // Hide UI first
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.hide();
@@ -153,19 +278,19 @@ public class FullscreenActivity extends AppCompatActivity {
         mControlsView.setVisibility(View.GONE);
         mVisible = false;
 
-        // Schedule a runnable to remove the status and navigation bar after a delay
+            // Schedule a runnable to remove the status and navigation bar after a delay
         mHideHandler.removeCallbacks(mShowPart2Runnable);
         mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
     }
 
     @SuppressLint("InlinedApi")
     private void show() {
-        // Show the system bar
-        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            // Show the system bar
+        videoView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
         mVisible = true;
 
-        // Schedule a runnable to display UI elements after a delay
+            // Schedule a runnable to display UI elements after a delay
         mHideHandler.removeCallbacks(mHidePart2Runnable);
         mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
     }
--- SubjectAndroid/app/src/main/java/com/example/haytran/subjectandroid/MainActivity.java
+++ SubjectAndroid/app/src/main/java/com/example/haytran/subjectandroid/MainActivity.java
@@ -2,54 +2,78 @@ package com.example.haytran.subjectandroid;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
     Button btnChangeActivity;
     VideoView videoView;
     Uri path;
     int currentPosition = 0;
+    String filename = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnChangeActivity = (Button)findViewById(R.id.btnChangeActivity);
-        videoView = (VideoView)findViewById(R.id.videoView);
-        Intent intent = getIntent();
-        path = intent.getData();
-        currentPosition = intent.getIntExtra("currentPositionLand",0);
-        String tmpPath = intent.getStringExtra("pathLandToPort");
-        Log.d("Hay*Nhung",tmpPath+"");
-        if(path != null) {
-            videoView.setVideoURI(path);
-            videoView.start();
-        }else if (tmpPath != null){
-            path = Uri.parse(intent.getStringExtra("pathLandToPort"));
-            videoView.setVideoURI(path);
-            videoView.seekTo(currentPosition);
-            videoView.start();
-        }
+        mapping();
+        init();
+        addControls();
+    }
+
+    private void addControls() {
         btnChangeActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent  intent = new Intent(MainActivity.this,FullscreenActivity.class);
-                intent.putExtra("pathPortToLand",path+"");
-                intent.putExtra("currentPositionPort",videoView.getCurrentPosition());
+                intent.putExtra("path",path+"");
+                intent.putExtra("currentPosition",videoView.getCurrentPosition());
+                intent.putExtra("filename",filename);
                 startActivity(intent);
+                finish();
             }
         });
+    }
+
+    private void init() {
+            // Request Storage Permission
         verifyStoragePermissions(this);
 
+            // Get info from intent
+        Intent intent = getIntent();
+            // Check whether video come from system or not
+        if ((path = intent.getData()) != null) {
+            videoView.setVideoURI(path);
+            filename = getFilename(path);
+            videoView.start();
+        } else if (intent.getFlags() == 105){
+            path = Uri.parse(intent.getStringExtra("path"));
+            currentPosition = intent.getIntExtra("currentPosition",0);
+            filename = intent.getStringExtra("filename");
+            videoView.setVideoURI(path);
+            videoView.seekTo(currentPosition);
+            videoView.start();
+        } else {
+            Toast.makeText(this, "Chưa có đường dẫn đến video", Toast.LENGTH_SHORT).show();
+        }
     }
+
+    private void mapping() {
+        btnChangeActivity = (Button)findViewById(R.id.btnChangeActivity_MainActivity);
+        videoView = (VideoView)findViewById(R.id.videoView_MainActivity);
+    }
+
     // Storage Permissions variables
     private static final int REQUEST_EXTERNAL_STORAGE = 1;
     private static String[] PERMISSIONS_STORAGE = {
@@ -57,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
             Manifest.permission.WRITE_EXTERNAL_STORAGE
     };
 
-    //persmission method.
+    // Request read and write in storage method
     public static void verifyStoragePermissions(Activity activity) {
         // Check if we have read or write permission
         int writePermission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
@@ -71,5 +95,28 @@ public class MainActivity extends AppCompatActivity {
                     REQUEST_EXTERNAL_STORAGE
             );
         }
+    }
+    public String getFilename(Uri uri)
+    {
+        /*  Intent intent = getIntent();
+            String name = intent.getData().getLastPathSegment();
+            return name;*/
+        String fileName = null;
+        Context context=getApplicationContext();
+        String scheme = uri.getScheme();
+        if (scheme.equals("file")) {
+            fileName = uri.getLastPathSegment();
+        }
+        else if (scheme.equals("content")) {
+            String[] proj = { MediaStore.Video.Media.TITLE };
+            Uri contentUri = null;
+            Cursor cursor = context.getContentResolver().query(uri, proj, null, null, null);
+            if (cursor != null && cursor.getCount() != 0) {
+                int columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.TITLE);
+                cursor.moveToFirst();
+                fileName = cursor.getString(columnIndex);
+            }
+        }
+        return fileName;
     }
 }
